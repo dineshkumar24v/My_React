51e6b59a16bfb2a316a07fb438590114562e09a5
dineshkumar24v <dineshkumar24v@gmail.com>
2025-07-26 18:44:34 +0530

    react Redux

---
+++ day29 react_redux/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>count</h1>
+      <button>Increment</button>  &nbsp; <button>Decrement</button>
+    </div>
+  )
+}
+
+export default App
---
+++ day29 react_redux/src/Redux/Reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  count: 0,
+};
+
+export const reducer = (state = initialState, actions) =>{
+  switch(actions) {
+    default:
+      return state;
+  }
+}
